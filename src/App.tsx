8dee3f6119de53473197946a9bf9804c05d9f91f
0libote <79215282+0libote@users.noreply.github.com>
2025-12-10 11:03:20 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { WeaponList } from './components/WeaponList';
 import { MasterySummary } from './components/MasterySummary';
