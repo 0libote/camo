b3e67a3ab5442300ad4ca958f43549e6d20f3e70
0libote <79215282+0libote@users.noreply.github.com>
2026-01-10 22:25:44 +0000

    Moving configuration files back to the root to restore layout stability.

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
   plugins: [react()],
   base: '/camo/', // GitHub Pages base URL
+  root: 'src',
   publicDir: '../public',
-  css: {
-    postcss: 'configs',
-  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
