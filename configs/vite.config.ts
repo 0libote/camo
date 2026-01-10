af8297d36a1e4875acb91e2396bcd0ee7d9e681c
0libote <79215282+0libote@users.noreply.github.com>
2026-01-10 22:20:48 +0000

    Module error fix

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '../src',
+  root: 'src',
   plugins: [react()],
   base: '/camo/', // GitHub Pages base URL
   publicDir: '../public',
-  css: {
-    postcss: '.',
-  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
